3f3e3556f32bdb02d0a4b984669c471e13a09410
Nikita <nikvs@mail.ru>
2022-11-27 18:49:32 +0700

    Corrected Task_1_7.java (Introduction_to_Java)

--- src/main/java/org/example/Task_1_7.java
+++ src/main/java/org/example/Task_1_7.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task_1_7 {
     public static void main(String[] args) {
 
@@ -17,6 +20,51 @@ public class Task_1_7 {
 
 
 //        УСЛОВИЕ ЗАДАНИЯ НЕ ПОНЯЛ. НЕОБХОДИМО УТОЧНИТЬ.
+
+//        function func (a, b :integer) :integer;
+//        begin
+//        if (b < a) then
+//        result:=0
+//else if (b = a) then result :=1
+//else result:=func(a + 1, b) + func(2 * a, b)
+//        end;
+//        begin
+//        writeln(func(2, 10) * func(10, 32));
+//        end.
+
+        System.out.println(func_java(2, 4));
+
+
+        int x = 1;
+        int y = 25;
+//        List<String> l = new ArrayList<>();
+
+        String l = "";
+        func_python(x, y, l);
+
+
+    }
+
+    public static int func_java(int a, int b) {
+        if (b < a) {
+            return 0;
+        } else if (b == a) {
+            return 1;
+        } else return (func_java(a + 1, b) + func_java(a * 2, b));
+    }
+
+    //    public static void func_python(int x, int y, List<String> l) {
+    public static void func_python(int x, int y, String l) {
+        if (x == y) {
+            System.out.println(l);
+        } else if (x < y) {
+//            l.add("k1");
+            func_python(x + 3, y, l + "K1 ");
+//            l.add("k2");
+            func_python(x * 2, y, l + "K2 ");
+        } else {
+            System.out.printf("");
+        }
 
     }
 }
